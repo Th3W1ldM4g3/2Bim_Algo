6c07056a685955262328ac1a59ef96dedd9717f3
Arthur Santiago Mayolino <arthursmpc@protonmail.com>
2025-05-26 19:23:40 +0000

    Add files via upload

---
+++ testando_definicoes.py
@@ -0,0 +1,31 @@
+# compras = ["pão", "café", "leite"]
+# print(compras)
+# compras.remove(compras[1]) #Pode remover tanto por nome quanto por índice
+# print(compras)
+# compras.append("açúcar") #Acrescenta 1 item no final de uma lista
+# print(compras)
+# compras.sort() #Organiza uma lista
+# print(compras)
+
+# # for panela in compras:
+# #     print(panela) #O nome do item identifica o  item na lista, esses itens podem receber qualquer nome
+    
+listas = []
+dicionarios = {}
+tuplas = ()
+conjuntos = set()
+
+# teste = [1,2,3,5]
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+# teste = (1,2,3,5)
+# print(teste)
+# teste.remove(1)
+# print(teste)
+
+teste = {1,2,3,5}
+print(teste)
+teste.add(4)
+print(teste)
